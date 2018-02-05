3e833709468cdcc3fd8fe3a1166b194b60fedd90
hhf <36131124+hhf42@users.noreply.github.com>
2018-02-05 17:48:30 +0100

    _
    _

--- serial_rb.cpp
+++ serial_rb.cpp
@@ -4,12 +4,12 @@
 
 //% weight=2 color=#002050 icon="\uf287"
 //% advanced=true
-namespace serial-rb {
+namespace serial_rb {
     // note that at least one // followed by % is needed per declaration!
-/**
-    * Reads multiple characters from the receive buffer and fills a user buffer.
-    * @param length default buffer length, eg: 64
-    */
+    /**
+     * Reads multiple characters from the receive buffer and fills a user buffer.
+     * @param length default buffer length, eg: 64
+     */
     //% blockId=serial_readbuffer block="serial|read buffer %length"
     //% help=serial/read-buffer advanced=true weight=5
     Buffer readBuffer(int length) {
