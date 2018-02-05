e75ea9d1d9fbc787c2511bdcdda34edea359cacc
hhf <36131124+hhf42@users.noreply.github.com>
2018-02-05 21:01:28 +0100

    Removed
    Removed
             export function readBuffer(length: number): Buffer;{
                return;
            }

--- serial_rb.ts
+++ serial_rb.ts
@@ -3,10 +3,6 @@ namespace serial_rb {
     * Reads multiple characters from the receive buffer and fills a user buffer.
     * @param length default buffer length, eg: 64
     */
-    //% weight=89 blockGap=8
     //% blockId=serial_readbuffer block="serial_rb|read buffer %length"
-    //% help=serial/read-buffer advanced=true weight=5
-        export function readBuffer(length: number): Buffer;{
-            return;
-        }
+    //% help=serial/read-buffer weight=5
     }
