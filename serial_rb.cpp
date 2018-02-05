93686789f593f6404685ae64deeda13b7b3d6cc6
hhf <36131124+hhf42@users.noreply.github.com>
2018-02-05 18:37:48 +0100

    Update serial_rb.cpp

--- serial_rb.cpp
+++ serial_rb.cpp
@@ -3,14 +3,13 @@
 #define MICROBIT_SERIAL_READ_BUFFER_LENGTH 64
 
 //% weight=2 color=#002050 icon="\uf287"
-//% advanced=true
 namespace serial_rb {
     // note that at least one // followed by % is needed per declaration!
     /**
      * Reads multiple characters from the receive buffer and fills a user buffer.
      * @param length default buffer length, eg: 64
      */
-    //% blockId=serial_readbuffer block="serial|read buffer %length"
+    //% blockId=serial_readbuffer block="serial_rb|read buffer %length"
     //% help=serial/read-buffer advanced=true weight=5
     Buffer readBuffer(int length) {
       if (length <= 0)
