2ec43fdd8b6017fbe0972380b88463a71944b5d9
hhf <36131124+hhf42@users.noreply.github.com>
2018-02-05 17:03:20 +0100

    Done
    Done

--- enums.d.ts
+++ enums.d.ts
@@ -1,20 +1,7 @@
 // Auto-generated. Do not edit.
 
 
-    declare enum ButtonState
-    {
-    BUTTON_UP = 0,
-    BUTTON_DOWN = 1,
-    }
-
-
-    declare enum MouseButton
-    {
-    MOUSE_BUTTON_LEFT = 0x1,
-    MOUSE_BUTTON_RIGHT = 0x2,
-    MOUSE_BUTTON_MIDDLE = 0x4,
-    }
-declare namespace bluetooth {
+declare namespace serial-rb {
 }
 
 // Auto-generated. Do not edit. Really.
