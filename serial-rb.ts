79144e4e1c61e4eef3c5c73f2f0b7f5c2f271ce4
hhf <36131124+hhf42@users.noreply.github.com>
2018-02-05 17:46:10 +0100

    Update serial-rb.ts

--- serial-rb.ts
+++ serial-rb.ts
@@ -2,9 +2,8 @@ namespace serial-rb {
     /**
     * Reads multiple characters from the receive buffer and fills a user buffer.
     * @param length default buffer length, eg: 64
-     */
+    */
     //% blockId=serial_readbuffer block="serial|read buffer %length"
     //% help=serial/read-buffer advanced=true weight=5
-    //% parts="serial"
-    //% advanced=true
+
     }
