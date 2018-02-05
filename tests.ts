4bb09596369e1bf6eb45cf86f6304ff19ecccbaa
hhf <36131124+hhf42@users.noreply.github.com>
2018-02-05 17:00:42 +0100

    Update tests.ts

--- tests.ts
+++ tests.ts
@@ -1,28 +1,4 @@
-bluetooth.onBluetoothConnected(() => {
-    basic.showString("C")
-})
-
-bluetooth.onBluetoothDisconnected(() => {
-    basic.showString("D")
-})
-
-bluetooth.startMouseService();
-basic.showString("S")
-
-input.onPinPressed(TouchPin.P0, () => {
-    bluetooth.setMouseButton(MouseButton.MOUSE_BUTTON_LEFT, ButtonState.BUTTON_DOWN)
-})
-input.onPinReleased(TouchPin.P0, () => {
-    bluetooth.setMouseButton(MouseButton.MOUSE_BUTTON_LEFT, ButtonState.BUTTON_UP)
-})
-
-input.onPinPressed(TouchPin.P1, () => {
-    bluetooth.setMouseButton(MouseButton.MOUSE_BUTTON_RIGHT, ButtonState.BUTTON_DOWN)
-})
-input.onPinReleased(TouchPin.P1, () => {
-    bluetooth.setMouseButton(MouseButton.MOUSE_BUTTON_RIGHT, ButtonState.BUTTON_UP)
-})
-
+let item: Buffer = null
 basic.forever(() => {
-    bluetooth.setMouseSpeed(input.acceleration(Dimension.X) / 8, input.acceleration(Dimension.Y) / 8, 0)
+    item = serial.readBuffer(64)
 })
