b33d10dd15e168910d63f63b4bac83be0ef22910
hhf <36131124+hhf42@users.noreply.github.com>
2018-02-05 17:58:10 +0100

    Unklar
    Unklar

--- serial_rb.ts
+++ serial_rb.ts
@@ -1,9 +1,12 @@
-namespace serial-rb {
+namespace serial_rb {
     /**
     * Reads multiple characters from the receive buffer and fills a user buffer.
     * @param length default buffer length, eg: 64
     */
+    //% weight=89 blockGap=8
     //% blockId=serial_readbuffer block="serial|read buffer %length"
     //% help=serial/read-buffer advanced=true weight=5
-
+        export function readBuffer(value: number): void {
+        readBuffer(value.toString());
+        }
     }
