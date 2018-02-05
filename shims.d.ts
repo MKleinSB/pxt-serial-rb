96609a807fff9eba448f3e5324bafcf24837ff67
hhf <36131124+hhf42@users.noreply.github.com>
2018-02-05 18:40:01 +0100

    Update shims.d.ts

--- shims.d.ts
+++ shims.d.ts
@@ -5,14 +5,13 @@
      * serial_rb
      */
     //% weight=2 color=#002050 icon="\uf287"
-    //% advanced=true
 declare namespace serial_rb {
         /**
          * Reads multiple characters from the receive buffer and fills a user buffer.
          * @param length default buffer length, eg: 64
          */
-        //% blockId=serial_readbuffer block="serial|read buffer %length"
-        //% help=serial/read-buffer advanced=true weight=5 shim=serial::readBuffer
+        //% blockId=serial_readbuffer block="serial_rb|read buffer %length"
+        //% help=serial/read-buffer weight=5 shim=serial_rb::readBuffer
         function readBuffer(length: number): Buffer;
 }
 
