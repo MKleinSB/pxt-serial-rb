dba2e1245b31571e267bf9d121b4eada0972b1c2
hhf <36131124+hhf42@users.noreply.github.com>
2018-02-05 17:28:07 +0100

    Done
    Done

--- shims.d.ts
+++ shims.d.ts
@@ -4,8 +4,71 @@
     /**
      * serial-rb
      */
-
+    //% weight=2 color=#002050 icon="\uf287"
+    //% advanced=true
 declare namespace serial-rb {
+        /**
+         * Reads multiple characters from the receive buffer and fills a user buffer.
+         * @param length default buffer length, eg: 64
+         */
+        //% blockId=serial_readbuffer block="serial|read buffer %length"
+        //% help=serial/read-buffer advanced=true weight=5 shim=serial::readBuffer
+        function readBuffer(length: number): Buffer;
+}
+
+    //% indexerGet=BufferMethods::getByte indexerSet=BufferMethods::setByte
+declare interface Buffer {
+    /**
+     * Write a number in specified format in the buffer.
+     */
+    //% shim=BufferMethods::setNumber
+    setNumber(format: NumberFormat, offset: number, value: number): void;
+
+    /**
+     * Read a number in specified format from the buffer.
+     */
+    //% shim=BufferMethods::getNumber
+    getNumber(format: NumberFormat, offset: number): number;
+
+    /** Returns the length of a Buffer object. */
+    //% property shim=BufferMethods::length
+    length: number;
+
+    /**
+     * Fill (a fragment) of the buffer with given value.
+     */
+    //% offset.defl=0 length.defl=-1 shim=BufferMethods::fill
+    fill(value: number, offset?: number, length?: number): void;
+
+    /**
+     * Return a copy of a fragment of a buffer.
+     */
+    //% offset.defl=0 length.defl=-1 shim=BufferMethods::slice
+    slice(offset?: number, length?: number): Buffer;
+
+    /**
+     * Shift buffer left in place, with zero padding.
+     * @param offset number of bytes to shift; use negative value to shift right
+     * @param start start offset in buffer. Default is 0.
+     * @param length number of elements in buffer. If negative, length is set as the buffer length minus start. eg: -1
+     */
+    //% start.defl=0 length.defl=-1 shim=BufferMethods::shift
+    shift(offset: number, start?: number, length?: number): void;
+
+    /**
+     * Rotate buffer left in place.
+     * @param offset number of bytes to shift; use negative value to shift right
+     * @param start start offset in buffer. Default is 0.
+     * @param length number of elements in buffer. If negative, length is set as the buffer length minus start. eg: -1
+     */
+    //% start.defl=0 length.defl=-1 shim=BufferMethods::rotate
+    rotate(offset: number, start?: number, length?: number): void;
+
+    /**
+     * Write contents of `src` at `dstOffset` in current buffer.
+     */
+    //% shim=BufferMethods::write
+    write(dstOffset: number, src: Buffer): void;
 }
 
 // Auto-generated. Do not edit. Really.
