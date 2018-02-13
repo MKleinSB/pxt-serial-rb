f9a21c746b5b3846fdca38f7f10389f117af7820
hhf <36131124+hhf42@users.noreply.github.com>
2018-02-13 22:37:17 +0100

    removed block definitions

--- serial_rb.ts
+++ serial_rb.ts
@@ -1,8 +1,2 @@
 namespace serial_rb {
-    /**
-    * Reads multiple characters from the receive buffer and fills a user buffer.
-    * @param length default buffer length, eg: 64
-    */
-    //% blockId=serial_readbuffer block="serial_rb|read buffer %length"
-    //% help=serial/read-buffer weight=5
     }
