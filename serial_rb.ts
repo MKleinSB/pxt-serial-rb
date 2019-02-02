ac10d0f11846d42da0600f5ab9cdc356b750d7fe
Michael Klein <37311672+MKleinSB@users.noreply.github.com>
2019-02-02 12:58:40 +0100

    Update serial_rb.ts

--- serial_rb.ts
+++ serial_rb.ts
@@ -1,6 +1,15 @@
-namespace serial_rb {
-    }
-
-    function writeBuffer(buff: buffer) : void {
-      if (!buffer) return;
+declare namespace serial_rb {
+        /**
+         * Reads multiple characters from the receive buffer and fills a user buffer.
+         * @param length default buffer length, eg: 64
+         */
+        //% blockId=serial_readbuffer block="serial_rb|read buffer %length"
+        //% help=serial/read-buffer weight=5 shim=serial_rb::readBuffer
+        function readBuffer(length: number): Buffer;
+        /**
+         * Sends a buffer through Serial connection
+         */
+        //% blockId=serial_writebuffer block="serial_rb|write buffer %buffer"
+        //% help=serial/write-buffer weight=6 shim=serial_rb::writeBuffer
+        function writeBuffer(buffer: Buffer): void;
 }
